9346193e844cba8bb90ef0f050c68964b7463144
Tomsylva <tomsylvanz@gmail.com>
2021-07-26 20:51:01 +0200

    font awesome cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,8 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     TaskItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
